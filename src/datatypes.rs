c231afeccd8b0d39c8fb27e2314a2ad35bd83a20
atcupps <91998663+atcupps@users.noreply.github.com>
2024-12-26 14:50:48 -0500

    Add datatype enums and structs

---
+++ src/datatypes.rs
@@ -0,0 +1,51 @@
+pub enum Type {
+    Char(u16),
+    Varchar(u16),
+    Tiny,
+    Short,
+    Int,
+    BigInt,
+    UnsignedTiny,
+    UnsignedShort,
+    UnsignedInt,
+    UnsignedBigInt,
+    Float,
+    Double,
+    Decimal(u8),
+    Bool,
+    Enum(String),   // Lookup by Enum name
+    Struct(String), // Lookup by Struct name
+}
+
+pub struct Schema {
+    types: Vec<Type>
+}
+
+pub enum Value {
+    Char(String),
+    Varchar(String),
+    Tiny(i8),
+    Short(i16),
+    Int(i32),
+    BigInt(i64),
+    UnsignedTiny(u8),
+    UnsignedShort(u16),
+    UnsignedInt(u32),
+    UnsignedBigInt(u64),
+    Float(f32),
+    Double(f64),
+    Decimal(i64, u8),
+    Bool(bool),
+    Enum(String, Vec<Value>),
+    Struct(Vec<Value>),
+}
+
+pub struct Tuple {
+    values: Vec<Value>
+}
+
+pub struct Table {
+    name: String,
+    schema: Schema,
+    tuples: Vec<Tuple>,
+}
